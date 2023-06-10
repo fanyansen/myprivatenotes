fe5ba4cf27f8a8ba60c4ddf8c440388db649a5f0
fanyansen <yansen@grip-principle.com>
2023-06-10 17:43:30 +0700

    First Config for react app with redux typescript

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+// import { RenderResult, render, Matcher } from "@testing-library/react";
+import { Matcher, screen } from "@testing-library/react";
+// import { Provider } from "react-redux";
+// import { store } from "./app/store";
+// import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  // const view = render(
+  //   <Provider store={store}>
+  //     <App />
+  //   </Provider>
+  // );
+
+  const expectedRes: Matcher = /GraphQL/i;
+
+  expect(screen.getByText(expectedRes)).toBeInTheDocument();
 });
---
+++ web/src/app/store.ts
@@ -0,0 +1,36 @@
+import {
+  configureStore,
+  combineReducers,
+  ThunkAction,
+  Action,
+} from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+export const persistor = persistStore(store);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
