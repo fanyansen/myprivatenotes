782e68d78ccef82227ec19d38e9f7172759e6f75
Fan Yansen <fanyansen@onebyone.io>
2025-09-28 23:37:46 +0700

    Improve Logic on Refresh Token Auth

--- apollo-gen-web/src/layout/MainLayout/index.tsx
+++ apollo-gen-web/src/layout/MainLayout/index.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Outlet } from "react-router";
-import { Navigate, useNavigate } from "react-router-dom";
-import { clearToken, isAuthenticated } from "../../lib/auth";
+import {
+  // Navigate,
+  useNavigate,
+} from "react-router-dom";
+import {
+  clearToken,
+  // isAuthenticated
+} from "../../lib/auth";
 import { useLogoutMutation } from "../../generated/graphql";
 
 function MainLayout() {
   const [submitLogout, { client, error }] = useLogoutMutation();
   const navigate = useNavigate();
 
-  const navigateToLoginPage = () => <Navigate to="/login" />;
+  // const navigateToLoginPage = () => <Navigate to="/login" />;
 
   const onLogoutHandler = async () => {
     await submitLogout();
@@ -19,9 +25,9 @@ function MainLayout() {
     // return navigateToLoginPage();
   };
 
-  if (!isAuthenticated()) {
-    return navigateToLoginPage();
-  }
+  // if (!isAuthenticated()) {
+  //   return navigateToLoginPage();
+  // }
 
   return (
     <>
--- apollo-gen-web/src/lib/api.ts
+++ apollo-gen-web/src/lib/api.ts
@@ -4,7 +4,9 @@ import {
   InMemoryCache,
   NormalizedCacheObject,
   createHttpLink,
+  fromPromise,
 } from "@apollo/client";
+import { ErrorLink } from "@apollo/client/link/error";
 import { setContext } from "@apollo/client/link/context";
 import { EXPRESS_URI, GRAPHQL_URI } from "../constants/env";
 import { getToken, isAuthenticated, saveToken } from "./auth";
@@ -12,6 +14,51 @@ import { getToken, isAuthenticated, saveToken } from "./auth";
 // import { getToken } from "./auth";
 import { MutationBaseOptions } from "@apollo/client/core/watchQueryOptions";
 import { TokenRefreshLink } from "apollo-link-token-refresh";
+
+const errorLink = new ErrorLink(
+  ({ graphQLErrors, networkError, operation, forward }) => {
+    if (graphQLErrors)
+      for (const err of graphQLErrors) {
+        switch (err.extensions.code) {
+          case "UNAUTHENTICATED":
+            return fromPromise(
+              fetch(`${EXPRESS_URI}/refresh-token`, {
+                method: "POST",
+                credentials: "include",
+              })
+                .then(async (response) => {
+                  const { access_token: newAccessToken } =
+                    await response.json();
+                  saveToken(newAccessToken);
+                  return newAccessToken;
+                })
+                .catch((error) => {
+                  // Handle token refresh errors e.g clear stored tokens, redirect to login
+
+                  saveToken("");
+                  window.location.href = "/login";
+                  return;
+                })
+            )
+              .filter((value) => Boolean(value))
+              .flatMap((accessToken) => {
+                const oldHeaders = operation.getContext().headers;
+                // modify the operation context with a new token
+                operation.setContext({
+                  headers: {
+                    ...oldHeaders,
+                    authorization: `Bearer ${accessToken}`,
+                  },
+                });
+
+                // retry the request, returning the new observable
+                return forward(operation);
+              });
+        }
+      }
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  }
+);
 
 const fetchOptions: MutationBaseOptions = { errorPolicy: "all" };
 
@@ -25,17 +72,22 @@ const refreshLink = new TokenRefreshLink({
     });
   },
   handleFetch: (accessToken) => saveToken(accessToken),
-  // handleResponse: (operation, accessTokenField) => () => {
-  //   // here you can parse response, handle errors, prepare returned token to
-  //   // further operations
-  //   // returned object should be like this:
-  //   // {
-  //   //    access_token: 'token string here'
-  //   // }
-  // },
+  handleResponse: (_, accessTokenField) => async (response: Response) => {
+    console.log("refreshLink - handleResponse - response", response);
+    const jsonRes = await response.json();
+    const { access_token, success } = jsonRes;
+    console.log("jsonRes", jsonRes);
+    if (!success) {
+      window.location.href = "/login";
+      throw new Error("Failed to refresh token");
+    }
+    return { [accessTokenField]: access_token };
+  },
   handleError: (err) => {
     // full control over handling token fetch Error
     console.warn("Your refresh token is invalid. Try to relogin");
+    saveToken("");
+    window.location.href = "/login";
     console.error(err);
   },
 });
@@ -54,6 +106,7 @@ const authLink = setContext((_, { headers }) => ({
 
 export const client = new ApolloClient<NormalizedCacheObject>({
   link: ApolloLink.from([refreshLink, authLink, httpLink]),
+  // link: ApolloLink.from([errorLink, authLink, httpLink]),
   // link: ApolloLink.from([authLink, httpLink]),
   cache: new InMemoryCache(),
   defaultOptions: {
